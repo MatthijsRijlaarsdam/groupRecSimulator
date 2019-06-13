eab84ce7f94f3e61c14e976aee90ef16f467ffdb
Matthijs Rijlaarsdam <matthijs@wlan-145-94-166-177.wlan.tudelft.nl>
2019-06-13 10:56:02 +0200

    added data_controller

---
+++ data_controller.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+class DataController:
+
+    def __init__(self):
+        #parse shit hier
+
+
+    def get_ratings_for_user(self,user):
+
+    def get_ratings_for_movie(self,movie):
+
+    def get_tags_for_movie(self,movie):
+
+    def get_tags_for_movie(self,movie,thresh):
+
+
+
